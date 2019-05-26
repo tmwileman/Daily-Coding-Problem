7e22a641e5811f0eec4d90c39acda554de6fca21
tmwileman <tmwilema@gmail.com>
2019-05-26 16:06:30 -0400

    Update Day 1.py

--- Day 1.py
+++ Day 1.py
@@ -4,6 +4,11 @@ Created on Sun May 26 15:52:53 2019
 
 @author: tmwil
 """
+# Good morning! Here's your coding interview problem for today.
+# This problem was recently asked by Google.
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+# Bonus: Can you do this in one pass?
 
 nums = [10, 15, 3, 7]
 k = 17
